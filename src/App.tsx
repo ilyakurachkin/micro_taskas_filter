ded6474e9219d6b79b506b9b37b928ec7cdfe7dc
ilyakurachkin <patlatov@inbox.ru>
2022-10-12 20:41:28 +0300

    filter practicle

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [money, setMoney] = useState([
+        {banknote: 'Dollars', nominal: 100, number: ' a1234567890'},
+        {banknote: 'Dollars', nominal: 50, number: ' z1234567890'},
+        {banknote: 'RUBLES', nominal: 100, number: ' w1234567890'},
+        {banknote: 'Dollars', nominal: 100, number: ' e1234567890'},
+        {banknote: 'Dollars', nominal: 50, number: ' c1234567890'},
+        {banknote: 'RUBLES', nominal: 100, number: ' r1234567890'},
+        {banknote: 'Dollars', nominal: 50, number: ' x1234567890'},
+        {banknote: 'RUBLES', nominal: 50, number: ' v1234567890'},
+    ])
+
+    let currentMoney = money.filter((FilterMoney)=>FilterMoney.banknote === 'RUBLES')
+
+
+    return (
+        <ul>
+            {currentMoney.map((objFromMoneyArr,index) => {
+                return (
+                    <li key={index}>
+                        <span>{objFromMoneyArr.banknote}</span>
+                        <span>{objFromMoneyArr.nominal}</span>
+                        <span>{objFromMoneyArr.number}</span>
+                    </li>
+                )
+            })}
+        </ul>
+    );
 }
 
 export default App;
